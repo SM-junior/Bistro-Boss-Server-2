16facb5387eb4c2d61eb4211409294d75e1ba263
SM-junior <shahinalam12341@gmail.com>
2024-02-09 01:21:51 +0600

    showing cart items number on navbar is done

--- index.js
+++ index.js
@@ -47,6 +47,18 @@ async function run() {
             res.send(result)
         })
 
+        app.get('/carts', async(req,res)=>{
+            const email=req.query.email;
+            if(!email){
+                res.send([])
+            }
+            else{
+                const query={email: email}
+                const result=await cartCollection.find(query).toArray();
+                res.send(result)
+            }
+        })
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
