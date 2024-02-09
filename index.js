7a04e2b63bb97f2dff2d7fb55fdf9131e7857701
SM-junior <shahinalam12341@gmail.com>
2024-02-09 15:00:35 +0600

    delete item from cart is working well

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.get('/', (req, res) => {
     res.send('server is running')
 })
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.PASS}@cluster0.n2wd0zs.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -58,6 +58,13 @@ async function run() {
                 res.send(result)
             }
         })
+
+        app.delete('/carts/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await cartCollection.deleteOne(query);
+            res.send(result)
+          })
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
